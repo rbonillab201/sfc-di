6e5f1780ccbac0fd01ab4b723fa6446f724fd52c
Renato Bonilla <renatio@gmail.com>
2020-10-03 16:28:07 -0600

    Proyecto Inicial

---
+++ src/main/java/net/cuscatlan/sfcdi/SfcDiApplication.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.sfcdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfcDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfcDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cuscatlan/sfcdi/SfcDiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.sfcdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfcDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
